ab86e7856965b6c3af70a3dacf175c9dd3b3c612
Myungwon Seo <seomyungwon@gmail.com>
2020-04-03 10:52:03 +0900

    MeSH raw data 추출 후 파일로 작성하는 method 추가

--- KEGG_OOP/Disease.py
+++ KEGG_OOP/Disease.py
@@ -227,9 +227,52 @@ class Disease:
 
         return Category_Info_List
 
-    #Disease code로부터 관련 MeSH id 정보 추출
+    def get_Disease_BRITE_Info(self, Disease_code):
 
+        DISEASE_INFO_URL = "http://rest.kegg.jp/get/" + Disease_code
+        BRITE_Info_List = []
+
+        key = False
+
+        try:
+            # HTTP Error 403: Forbidden 에러 해결법 (봇 형태의 접근을 막는 에러)
+            # 아래와 같이 코드를 작성하고 headers를 추가하면 됨
+
+            headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)' 
+                      'AppleWebKit/537.11 (KHTML, like Gecko) '
+                      'Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                       'Accept-Encoding': 'none',
+                       'Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'}
+
+            req = urllib.request.Request(DISEASE_INFO_URL, headers=headers)
+            response = urllib.request.urlopen(req).readlines()
+
+            print(response)
+            #for info in response:
+
+                #if str(info).__contains__()
+
+
+        except HTTPError as e:
+            print("HTTPError")
+            print(e)
+
+        except TimeoutError as e:
+            print("Timeout Error")
+            print(e)
+
+        except socket.timeout:
+            print("Timeout")
+
+        except Exception as e:
+            print(e)
+
+        return BRITE_Info_List
+
+    #Disease code로부터 관련 MeSH id 정보 추출
     def get_Disease_MeSH_id_Info(self, Disease_code):
+
         DISEASE_INFO_URL = "http://rest.kegg.jp/get/" + Disease_code
 
         Temp_List = []
--- Main/Main_MeSH.py
+++ Main/Main_MeSH.py
@@ -10,14 +10,17 @@ Data_r = Data_Reader()
 MeSH_f = MeSH_Finder()
 Data_w = Data_Writer()
 
-FilePath = "C:\\Users\\seomy\\Dropbox\\Seomyungwon\\#Multi_Level_Hyper_Network\\Data\\DB_data_process\\MeSH\\MeSH_raw\\"
+FilePath = "C:\\Users\\seomy\\Dropbox\\Seomyungwon\\#Multi_Level_Hyper_Network\\#Data\\DB_data_process\\MeSH\MeSH_raw\\"
 FileName = "desc2020.xml"
 
-DataFilePath = "C:\\Users\\seomy\\Dropbox\\Seomyungwon\\#Multi_Level_Hyper_Network\\Data\\DB_data_process\\MeSH_mapping\\Input\\"
-DataFileName = "DisGeNet_Disease_nonMapping_list7.txt"
+#DataFilePath = "C:\\Users\\seomy\\Dropbox\\Seomyungwon\\#Multi_Level_Hyper_Network\\Data\\DB_data_process\\MeSH_mapping\\Input\\"
+#DataFileName = "DisGeNet_Disease_nonMapping_list7.txt"
 
-OutputFilePath = "C:\\Users\\seomy\\Dropbox\\Seomyungwon\\#Multi_Level_Hyper_Network\\Data\\DB_data_process\\MeSH_mapping\\Output\\"
-OutputFileName = "DisGeNet_nonMapping_data_Unique_ID_mapping_result_2020_7.txt"
+#OutputFilePath = "C:\\Users\\seomy\\Dropbox\\Seomyungwon\\#Multi_Level_Hyper_Network\\Data\\DB_data_process\\MeSH_mapping\\Output\\"
+#OutputFileName = "DisGeNet_nonMapping_data_Unique_ID_mapping_result_2020_7.txt"
+
+OutputFilePath_temp = "C:\\Users\\seomy\\Desktop\\"
+OutputFileName_temp = "MeSH_RawData.txt"
 
 
 MeSH_All_Name = MeSH_r.get_All_MeSH_Concept_Term_Names(FilePath, FileName)
@@ -27,6 +30,7 @@ MeSH_Unique_ID = MeSH_r.get_All_MeSH_Unique_ID(FilePath, FileName)
 print(len(MeSH_All_Name))
 print(len(MeSH_Unique_ID))
 
+"""
 #Input disease name 정보
 Input_Disease_Name = Data_r.read_Disease_Name_txt(DataFilePath, DataFileName, 0)
 
@@ -40,3 +44,7 @@ MeSH_Unique_ID_Mapping = MeSH_f.find_MeSH_Disease_Cocept_Term_To_ID(Input_Diseas
 
 # File write
 Data_w.write_disease_to_id_file_txt(OutputFilePath, OutputFileName, Input_Disease_Name, MeSH_Unique_ID_Mapping)
+"""
+
+#Write MeSH raw data
+Data_w.write_MeSH_Info_txt(OutputFilePath_temp, OutputFileName_temp, MeSH_All_Name, MeSH_Unique_ID)
