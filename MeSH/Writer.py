8b35b17afd397400be6f14bc26853ec681c23b99
Myungwon Seo <seomyungwon@gmail.com>
2020-04-03 10:22:12 +0900

    MeSH raw data 추출 후 파일로 작성하는 method 추가

--- MeSH/Writer.py
+++ MeSH/Writer.py
@@ -16,6 +16,22 @@ class Data_Writer:
 
         print("Write file")
 
+    def write_MeSH_Info_txt(self, OutputFilePath, OutputFileName, All_MeSH_Term_list, All_MeSH_Unique_ID_list):
+
+        Final_data_string = 'MeSH_Term' + "\t" + "MeSH_Unique_ID" + "\n"
+
+        f = open(OutputFilePath + OutputFileName, 'w')
+
+        for ai in range(0, len(All_MeSH_Term_list)):
+
+            Final_data_string = Final_data_string + str(All_MeSH_Term_list[ai] + "\t" + str(All_MeSH_Unique_ID_list[ai] + "\n"))
+
+        f.write(Final_data_string)
+        f.close()
+
+        print("Write MeSH raw data")
+
+
 """
     def write_disease_to_All_id_file_txt(self, OutputFilePath, OutputFileName, Input_Name_list, MeSH_Unique_ID_list, MeSH_xml_total_Unique_ID_list):
 
