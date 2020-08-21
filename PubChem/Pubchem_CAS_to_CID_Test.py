2e97755821c3c5791be82ca0f9068154cb9b50d7
Myungwon Seo <seomyungwon@gmail.com>
2020-08-21 16:28:43 +0900

    edit

--- PubChem/Pubchem_CAS_to_CID_Test.py
+++ PubChem/Pubchem_CAS_to_CID_Test.py
@@ -1,26 +1,40 @@
 from urllib.request import urlopen
 
 
+Data = []
 Biocide_CAS = []
-
+Biocide_Err = []
 
 #read biocide compound CAS number file
-f = open("C:\\Users\\hkjin\\Desktop\\Python_Calculation\\Extract_CAS_Number_v2.txt", 'r')
+f = open("C:\\Users\\hkjin\\Desktop\\Python_Calculation\\Biocide_List_920_Test.txt", 'r')
 
 while True:
     line = f.readline()
     if not line:break
     new_line = line.split("\t")
-    Biocide_CAS.append(new_line[0].strip())
+    Data.append(new_line[0].strip())
 
 print("Read CAS number files")
 
+print(len(Data))
+
+for d in Data:
+    if(d.__contains__("-")):
+        Biocide_CAS.append(d)
+        #print(d)
+    else:
+        Biocide_Err.append(d)
+
+print(str("CAS: ") + str(len(Biocide_CAS)))
+print(str("Err: ") + str(len(Biocide_Err)))
 
 
 Pubchem_CID = []
 Biocide_CAS_of_CID = []
 
 for cas_num in Biocide_CAS:
+
+    Label = False
 
     print(str("Input CAS number: ") + str(cas_num).strip())
 
@@ -44,33 +58,49 @@ for cas_num in Biocide_CAS:
                   replace(">","").
                   replace("\\n","").strip())
 
+            #print(str(PubChem_line).
+            #      replace("b'","").
+            #      replace('"','').
+            #      replace("    <meta name=pubchem_uid_value content=","").
+            #      replace("'","").
+            #      replace(">","").
+            #      replace("\\n","").strip())
+
             Biocide_CAS_of_CID.append(cas_num)
-            """
-            print(str(PubChem_line).
-                  replace("b'","").
-                  replace('"','').
-                  replace("    <meta name=pubchem_uid_value content=","").
-                  replace("'","").
-                  replace(">","").
-                  replace("\\n","").strip())
-            """
+
+            Label = True
+            break
+
+    if(Label == False):
+
+        Biocide_CAS_of_CID.append("None")
 
 
+
+
+
+
+
+
+print(len(Biocide_CAS_of_CID))
 #print(len(Biocide_CAS))
 #print(len(Pubchem_CID))
 
+for info in Biocide_CAS_of_CID:
+    print(info)
 
+"""
 print("Extract CID number")
 
 f_out = open("C:\\Users\\hkjin\\Desktop\\Python_Calculation\\Extract_CAS_CID_output_new_v2.txt", "w")
 
 #Data = str("CAS") + "\t" + str("PubChem_CID") + "\n"
-"""
-for ai in range(0, len(Biocide_CAS)):
 
-    Data = str(Biocide_CAS[ai]) + "\t" + str(Pubchem_CID[ai]) + "\n"
-    f_out.write(Data)
-"""
+#for ai in range(0, len(Biocide_CAS)):
+
+#    Data = str(Biocide_CAS[ai]) + "\t" + str(Pubchem_CID[ai]) + "\n"
+#    f_out.write(Data)
+
 for bi in range(0, len(Pubchem_CID)):
 
     Data = str(Biocide_CAS_of_CID[bi]) + "\t" + str(Pubchem_CID[bi]) + "\n"
@@ -78,3 +108,6 @@ for bi in range(0, len(Pubchem_CID)):
 
 f_out.close()
 print("Write files")
+
+
+"""
